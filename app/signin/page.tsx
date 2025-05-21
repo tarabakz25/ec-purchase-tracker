e761f35b0d97f9aa30c59c6cc084b4750dc0b550
tarabakz25 <aikikizuki@gmail.com>
2025-05-22 00:29:46 +0900

    ミドルウェアの削除、Radix UI ライブラリの追加、レイアウトの修正、サインインページのUI改善を行いました。

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,18 +2,25 @@ import { useState, FormEvent } from "react";
 import { supabase } from "@/lib/supabase";
 
 const signIn = () => {
-    const handleLogin = async () => {
-        const { data, error } = await supabase.auth.signInWithOAuth({
-            provider: 'google',
-        })
-        if (error) {
-            console.error(error);
-        } else {
-            window.location.href = data.url;
-        }
+  const handleLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+    if (error) {
+			console.error(error);
+    } else {
+      window.location.href = data.url;
     }
+  };
 
-    return (
-        
-    )
+	return (
+		<div className="flex flex-col items-center justify-center h-screen">
+			<div>
+				<h1 className="text-2xl font-bold">Sign in</h1>
+			</div>
+			<div className="flex flex-col items-center justify-center">
+				<button onClick={handleLogin}>Sign in with Google</button>
+			</div>
+		</div>
+	)
 }
